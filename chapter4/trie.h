2523f6ec883386fcdc3d1986f614c4f1fa058b3c
liuyang64 <liuyang64@baidu.com>
2015-04-30 17:56:54 +0800

    Add trie

--- chapter4/trie.h
+++ chapter4/trie.h
@@ -8,23 +8,28 @@
 #include <string>
 
 namespace crack {
-class DictTrie {
+class Trie {
     typedef std::vector<TrieNode*> TrieNodes;
-    tyopo                       
+    tyopo
     class TrieNode {
     public:
         explicit TrieNode(char value) : _value(value), _parent(nullptr) {}
-        TrieNode() : _value(NULL), _parent(nullptr) {};
+        TrieNode() : _value(NULL), _parent(nullptr) {}
         TrieNode(char value, const std::vector<TrieNode*>& kids, const Node* parent) _value(value), _kids(kids), _parent(parent) {}
-        const std::vector<TrieNode>get_kids() {return _kids}
+        const std::vector<TrieNode>get_kids() { return _kids; }
     private:
         char _value;
         std::vector<TrieNode*> _kids;
         TrieNode* _parent;
     };
-    explicit DictTrie(std::vector<std::string>);
-    virtual ~DictTrie();
-    
+public:
+    explicit Trie(std::vector<std::string>);
+    virtual ~Trie();
+    void free_nodes();
+    void bfs_traversal();
+    void dfs_traversal();
+
+private:
+
 };
 } // namespace crack
-
