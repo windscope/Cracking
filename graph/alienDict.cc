0db01f531ff56428bafff72aaea1d046dbc14112
Yang Liu <yangliu@sh.fung.hku@gmail.com>
2016-08-16 17:31:30 -0700

    Add new problem

--- graph/alienDict.cc
+++ graph/alienDict.cc
@@ -6,79 +6,78 @@
 
 using namespace std;
 
+
 class Solution {
+
 public:
     string alienOrder(vector<string>& words) {
-        // build the graph
+        if (words.size() < 1) {
+            return {};
+        }
         if (words.size() == 1) {
             return words.front();
         }
         unordered_map<char, unordered_set<char>> graph;
-        for (int i = 1; i < words.size(); ++i) {
-            const string& t1 = words[i - 1];
-            const string& t2 = words[i];
+        for (size_t i = 1; i < words.size(); ++i) {
+            auto& prev = words[i-1];
+            auto& cur = words[i];
             bool changed = false;
-            for (int i = 0; i < t1.size() || i < t2.size(); ++i) {
-                if (i < t1.size() && graph.count(t1[i]) < 1) {
-                    graph.insert({t1[i], {}});
+            for (size_t j = 0; j < cur.size() || j < prev.size(); ++j) {
+                if (j < prev.size() && graph.count(prev[j]) < 1) {
+                    graph[prev[j]] = {};
                 }
-                if (i < t2.size() && graph.count(t2[i]) < 1) {
-                    graph.insert({t2[i], {}});
+                if (j < cur.size() && graph.count(cur[j]) < 1) {
+                    graph[cur[j]] = {};
                 }
-                if (!changed && t1[i] != t2[i]) {
+                if (!changed && j < cur.size() && j < prev.size() && cur[j] != prev[j]) {
+                    graph[prev[j]].insert(cur[j]);
                     changed = true;
-                    graph[t1[i]].insert(t2[i]);
                 }
             }
         }
-        return top_sort(graph);
-    }
-private:
-    string top_sort(unordered_map<char, unordered_set<char>>& graph) {
-        unordered_set<char> visited_nodes;
-        unordered_set<char> cycle_detector;
-        string ret;
-        for (auto& node_pair : graph) {
-            cout << node_pair.first << endl;
-            if (!dfs(graph, visited_nodes, cycle_detector, node_pair.first, ret)) {
-                return "";
+        vector<char> cycle(26, false);
+        vector<char> visit(cycle);
+        string ret{};
+        function<bool(const char)> dfs = [&dfs, &ret, &cycle, &visit, &graph] (const char c) {
+            int key = c - 'a';
+            if (cycle[key]) {
+                return false;
+            }
+            if (visit[key]) {
+                return true;
+            }
+            cycle[key] = true;
+            for (const auto nc : graph[c]) {
+                if (!dfs(nc)) {
+                    return false;
+                }
+            }
+            ret.push_back(c);
+            visit[key] = true;
+            cycle[key] = false;
+            return true;
+        };
+        for (const auto& p : graph) {
+            if (!dfs(p.first)) {
+                return {};
             }
         }
         reverse(ret.begin(), ret.end());
         return ret;
     }
-
-    bool dfs(unordered_map<char, unordered_set<char>>& graph,
-            unordered_set<char>& visited_nodes,
-            unordered_set<char>& cycle_detector,
-            const char node,
-            string& sorted_nodes) {
-        if (cycle_detector.count(node) > 0) {
-            return false;
-        }
-        if (visited_nodes.count(node) > 0) {
-            return true;
-        }
-        cycle_detector.insert(node);
-        visited_nodes.insert(node);
-
-        for (auto it = graph[node].begin(); it != graph[node].end(); ++it) {
-            if (!dfs(graph, visited_nodes, cycle_detector, *it, sorted_nodes)) {
-                return false;
-            }
-        }
-        cycle_detector.erase(node);
-        sorted_nodes.push_back(node);
-        return true;
-    }
-
-
 };
 
-
 int main(void) {
-    vector<string> data = {"wnlb"};
+    vector<vector<string>> data = {
+        {"wrt","wrf","er","ett","rftt"},
+        {"wnlb"},
+        {},
+        {"a", "b", "a"},
+        {"fzhwdxo","lg","gu","ntndzbhnud","wiehfmh","itetkyeqwq","rp","wrbx","upjx","quhnr","kf","onv","ycjlkl","te","mtvzh","b","bk","q","lqme","iaprvi"}
+    };
     Solution s;
-    cout << s.alienOrder(data) << endl;
+    for (auto& d : data) {
+        cout << s.alienOrder(d) << endl;
+    }
     return 0;
 }
